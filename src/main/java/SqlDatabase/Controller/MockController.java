6aaeb0118bf3fadb02e81f953468a67b9e628c16
alexb454 <71828880+alexb454@users.noreply.github.com>
2021-12-16 12:24:25 -0330

    done

--- src/main/java/SqlDatabase/Controller/MockController.java
+++ src/main/java/SqlDatabase/Controller/MockController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:8087")
+@CrossOrigin(origins = "http://localhost:8083")
 @RestController
 @RequestMapping("/api")
 public class MockController {
